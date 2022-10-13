f3f9084339eae0ea8e7ac5daddc5b20c7a69778b
franklin ikegbunam <chukwudiikegbunam@gmail.com>
2022-10-13 16:11:26 -0700

    Commit it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,11 @@
 /**
  *  * main - Entry point
  *   * Description: C program that prints a function followed by a new line
- *    * Return: 0 if successful*/
+ *    * Return: 0 if successful
+ *     */
 
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-}
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}}
