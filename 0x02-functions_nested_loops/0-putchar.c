7a566546747a2a919effe3460fb9d76c77196bde
franklin ikegbunam <chukwudiikegbunam@gmail.com>
2022-10-18 14:14:34 -0700

    Commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,23 @@
 #include "main.h"
+
 /**
- *  * main - function that prints _putchar
- *   * Return: returns 0 if successful
- *    */
+ *  * main - prints Putchar
+ *   *
+ *    * Description: prints _putchar
+ *     *
+ *      * Return: 0 (Success)
+ *       */
 
 int main(void)
 {
-	    _putchar('_');
-	        _putchar('p');
-		    _putchar('u');
-		        _putchar('t');
-			    _putchar('c');
-			        _putchar('h');
-				    _putchar('a');
-				        _putchar('r');
-					    _putchar('\n');
-					        return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return(0);
 }
